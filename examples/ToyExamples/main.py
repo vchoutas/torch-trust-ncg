aa1ecbadeb8561e8cacf311e19d7fed29140684a
vchoutas <vchoutas@users.noreply.github.com>
2022-02-25 15:47:13 +0100

    Adapt examples to use correct imports

--- examples/ToyExamples/main.py
+++ examples/ToyExamples/main.py
@@ -1,6 +1,9 @@
 import torch
-from Code.TrustRegion import TrustRegion
-from Utils.utils import rosenbrock, branin
+from torchtrustncg import TrustRegion
+from torchtrustncg.utils import rosenbrock, branin
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     import sys
@@ -11,15 +14,21 @@ if __name__ == "__main__":
                         help='Test function to optimize')
     parser.add_argument('--num-iters', dest='num_iters', default=200, type=str,
                         help='Number of iterations to use')
-    parser.add_argument('--gtol', default=1e-4, type=float, help='Gradient tolerance')
+    parser.add_argument('--gtol', default=1e-4, type=float,
+                        help='Gradient tolerance')
     parser.add_argument('--init-point', nargs=2, default=[2.5, 2.5], type=float, dest='init_point',
                         help='Point to initialize the optimization')
+
+    parser.add_argument('--plot',
+                        type=lambda x: x.lower() in ['true', '1'],
+                        help='Plot the function trajectory')
 
     args = parser.parse_args()
     function = args.function
     num_iters = args.num_iters
     gtol = args.gtol
     init_point = args.init_point
+    plot = args.plot
 
     if function == 'rosenbrock':
         loss_func = rosenbrock
@@ -36,7 +45,6 @@ if __name__ == "__main__":
 
     optimizer = TrustRegion([variable], opt_method='krylov')
 
-
     def closure(backward=True):
         if backward:
             optimizer.zero_grad()
@@ -45,11 +53,17 @@ if __name__ == "__main__":
             loss.backward(create_graph=True)
         return loss
 
+    points = []
+    values = []
 
     for n in range(num_iters):
         loss = optimizer.step(closure)
 
         np_var = variable.detach().cpu().numpy().squeeze()
+
+        if plot:
+            points.append(np_var.copy())
+            values.append(loss.item())
 
         if torch.norm(variable.grad).item() < gtol:
             break
@@ -60,3 +74,20 @@ if __name__ == "__main__":
             f'[{n:04d}]: ' +
             f'Loss at ({variable[0, 0]:.4f}, {variable[0, 1]:.4f}) = ' +
             f'{loss.item():.4f}')
+
+    if plot:
+        N = 100
+        X, Y = np.meshgrid(
+            np.linspace(-4 + init_point[0], init_point[0] + 4, N),
+            np.linspace(-4 + init_point[1], init_point[1] + 4, N)
+        )
+
+        grid_points = np.stack([X, Y], axis=-1).reshape(-1, 2)
+        Z = loss_func(torch.from_numpy(grid_points)).detach().numpy()
+
+        cs = plt.contourf(X, Y, Z.reshape(N, N))
+
+        points = np.stack(points, axis=0)
+
+        plt.plot(points[:, 0], points[:, 1], 'x-', color='black')
+        plt.show()
